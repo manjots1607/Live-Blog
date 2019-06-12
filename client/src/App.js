71ac670cda7c7afc9cde0afd00f3c2e1dc4d287a
BHARAT <nischalbharat4819@gmail.com>
2019-06-12 19:55:08 +0530

    signin signup added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
 import './App.css';
+import Signin from './components/auth/signinForm';
+import Signup from './components/auth/signupForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+
+        <Switch>
+          <Route exact path="/signin" component={Signin}/>
+          <Route exact path="/" component={Signup}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
