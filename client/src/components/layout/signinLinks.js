e8003c833b0c1132bea77182082a17d40f80a13c
BHARAT <nischalbharat4819@gmail.com>
2019-07-16 20:29:50 +0530

    nav element position fixed

--- client/src/components/layout/signinLinks.js
+++ client/src/components/layout/signinLinks.js
@@ -53,7 +53,6 @@ const SignedInLinks = (props)=>{
     };
     return (
             [<li className="nav-item mx-2"><NavLink className="nav-link text-white" to="/createBlog">Add Story</NavLink></li>,
-            <li className="nav-item mx-2"><NavLink className="nav-link text-white" onClick={handleSignOut} to="/">LogOut</NavLink></li>,
             <li className="nav-item dropdown mx-2">
               <a className="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 {props.user.name}
@@ -63,7 +62,9 @@ const SignedInLinks = (props)=>{
                 <a className="dropdown-item" href="#" onClick={handleFollowing}>Following</a>
                 <a className="dropdown-item" href="#" onClick={handleBookmarks}>Bookmarks</a>
               </div>
-            </li>]
+            </li>,
+            <li className="nav-item mx-2"><NavLink className="nav-link text-white" onClick={handleSignOut} to="/">LogOut</NavLink></li>
+          ]
         );
 };
 
