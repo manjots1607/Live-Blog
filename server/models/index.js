437a3dc669a253e4050249218909334e0afd8325
manjots1607 <manjotsingh16july@gmail.com>
2019-07-17 16:46:57 +0530

    log added database

--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,9 @@
 const mongoose=require("mongoose");
 mongoose.set("debug",true);
 mongoose.Promise=Promise;
-
+console.log(process.env.DATABASEURL);
 const databaseURL=process.env.DATABASEURL||"mongodb://localhost/liveBlog";
-
+console.log(databaseURL);
 mongoose.connect(databaseURL,{
     useNewUrlParser:true
 })
