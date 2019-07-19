8e3e222151297b155d35aca686f727b69e1280fe
manjots1607 <manjotsingh16july@gmail.com>
2019-07-19 13:25:37 +0530

    mail config updated

--- server/mail.js
+++ server/mail.js
@@ -5,12 +5,12 @@ var transporter = nodemailer.createTransport({
     secure: true,
     auth: {
         type: 'OAuth2',
-        user: 'manjotsingh16july@gmail.com',
-        clientId: '825656477336-78d1p99vljedfpm2bv181ntd1b1obta7.apps.googleusercontent.com',
-        clientSecret: 'gRedgJh5F_nZjI0i_j6BgR84',
-        refreshToken: '1/ajJoXfqaj-3xkDxJzJDS2mdzDoO5vx4x4ax_OywuYY9TPqSPPBYcA1LaaVQ5P-ch',
-        accessToken: 'ya29.GlvNBvjBJsFkOlstDauW45RYYEPEw270ZpwLhyWFlDEFEqwFNgPx9GPoWjmPuEOsrOCbXuXwon6y-rDD2cjIK7uWw9LhHaaOpTYrxJ4w11ATrSeEj-R4JK0LJRkh',
-        expires: 1484314697598
+        user: process.env.mail_user,
+        clientId: process.env.mail_client_id,
+        clientSecret: process.env.mail_client_secret,
+        refreshToken: process.env.mail_refresh_token,
+        accessToken: process.env.mail_access_token,
+        expires: Number(process.env.expires)
     }
 });
 
